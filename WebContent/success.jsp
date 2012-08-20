513a852f216e211327d4bb39ab80fa90d597233e
JeanZ <970263026@qq.com>
2012-08-20 17:24:28 +0800

    first commit

---
+++ WebContent/success.jsp
@@ -0,0 +1,9 @@
+<html>
+
+<head></head>
+
+<body>
+<h1>ssh demo success</h1>
+</body>
+
+</html>
---
+++ src/action/DemoAction.java
@@ -0,0 +1,20 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DemoAction extends ActionSupport {
+
+	/**
+	 * @author jean
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public String execute() throws Exception {
+
+		// do nothing
+		
+		return SUCCESS;
+		
+	}
+
+}
